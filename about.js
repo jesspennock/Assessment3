a44d7c4ad76b07467d27057992b797d72a925e48
Jess Pennock <jess.pennock@gmail.com>
2022-09-30 08:24:44 -0600

    about.js code added

--- about.js
+++ about.js
@@ -5,10 +5,14 @@ console.log("hello world");
 function handleSubmit(evt) {
 	evt.preventDefault();
 	
-	console.log('form submit');
+	alert("Form has been submitted successfully!");
 }
 
-
+const complimentUser = (event) => {
+	alert("You are prettier than this crystal covered ducky.")
+}
 let form = document.querySelector('#contact');
 
 form.addEventListener('submit', handleSubmit);
+let blingDuck = document.querySelector("#rubberducky");
+blingDuck.addEventListener("mouseover", (complimentUser))
